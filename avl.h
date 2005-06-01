cd4b589efc8b899ea0a54f80e09b44f2462fd9c9
Sam Rushing <sam-github@rushing.nightmare.com>
2005-06-01 22:33:16 +0000

    add an "avl_" prefix to all global functions and macros

--- avl.h
+++ avl.h
@@ -1,8 +1,9 @@
 /*
  * Copyright (C) 1995 by Sam Rushing <rushing@nightmare.com>
+ * Copyright (C) 2005 by Germanischer Lloyd AG
  */
 
-/* $Id: avl.h,v 2.3 1997/02/21 23:27:19 rushing Exp rushing $ */
+/* $Id: avl.h,v 2.4 2005/06/01 22:30:12 rushing Exp rushing $ */
 
 #ifdef __cplusplus
 extern "C" {
@@ -21,15 +22,15 @@ typedef struct avl_node_tag {
   unsigned long         rank_and_balance;
 } avl_node;
 
-#define GET_BALANCE(n)  ((int)(((n)->rank_and_balance & 3) - 1))
+#define AVL_GET_BALANCE(n)      ((int)(((n)->rank_and_balance & 3) - 1))
 
-#define GET_RANK(n)     (((n)->rank_and_balance >> 2))
+#define AVL_GET_RANK(n) (((n)->rank_and_balance >> 2))
 
-#define SET_BALANCE(n,b) \
+#define AVL_SET_BALANCE(n,b) \
   ((n)->rank_and_balance) = \
     (((n)->rank_and_balance & (~3)) | ((int)((b) + 1)))
 
-#define SET_RANK(n,r) \
+#define AVL_SET_RANK(n,r) \
   ((n)->rank_and_balance) = \
     (((n)->rank_and_balance & 3) | (r << 2))
 
@@ -54,44 +55,44 @@ typedef struct _avl_tree {
   void *                        compare_arg;
 } avl_tree;
 
-avl_tree * new_avl_tree (avl_key_compare_fun_type compare_fun, void * compare_arg);
-avl_node * new_avl_node (void * key, avl_node * parent);
+avl_tree * avl_new_avl_tree (avl_key_compare_fun_type compare_fun, void * compare_arg);
+avl_node * avl_new_avl_node (void * key, avl_node * parent);
 
-void free_avl_tree (
+void avl_free_avl_tree (
   avl_tree *            tree,
   avl_free_key_fun_type free_key_fun
   );
 
-int insert_by_key (
+int avl_insert_by_key (
   avl_tree *            ob,
   void *                key
   );
 
-int remove_by_key (
+int avl_remove_by_key (
   avl_tree *            tree,
   void *                key,
   avl_free_key_fun_type free_key_fun
   );
 
-int get_item_by_index (
+int avl_get_item_by_index (
   avl_tree *            tree,
   unsigned long         index,
   void **               value_address
   );
 
-int get_item_by_key (
+int avl_get_item_by_key (
   avl_tree *            tree,
   void *                key,
   void **               value_address
   );
 
-int iterate_inorder (
+int avl_iterate_inorder (
   avl_tree *            tree,
   avl_iter_fun_type     iter_fun,
   void *                iter_arg
   );
 
-int iterate_index_range (
+int avl_iterate_index_range (
   avl_tree *            tree,
   avl_iter_index_fun_type iter_fun,
   unsigned long         low,
@@ -99,14 +100,14 @@ int iterate_index_range (
   void *                iter_arg
   );
 
-int get_span_by_key (
+int avl_get_span_by_key (
   avl_tree *            tree,
   void *                key,
   unsigned long *       low,
   unsigned long *       high
   );
 
-int get_span_by_two_keys (
+int avl_get_span_by_two_keys (
   avl_tree *            tree,
   void *                key_a,
   void *                key_b,
@@ -114,26 +115,26 @@ int get_span_by_two_keys (
   unsigned long *       high
   );
 
-int verify (avl_tree * tree);
+int avl_verify (avl_tree * tree);
 
-void print_tree (
+void avl_print_tree (
   avl_tree *            tree,
   avl_key_printer_fun_type key_printer
   );
 
-avl_node * get_predecessor (avl_node * node);
+avl_node * avl_get_predecessor (avl_node * node);
 
-avl_node * get_successor (avl_node * node);
+avl_node * avl_get_successor (avl_node * node);
 
 /* These two are from David Ascher <david_ascher@brown.edu> */
 
-int get_item_by_key_most (
+int avl_get_item_by_key_most (
   avl_tree *            tree,
   void *                key,
   void **               value_address
   );
 
-int get_item_by_key_least (
+int avl_get_item_by_key_least (
   avl_tree *            tree,
   void *                key,
   void **               value_address
