2759af94cf1cb9e1d95094515e1a18d25732a662
Sam Rushing <sam-github@rushing.nightmare.com>
2005-06-02 01:17:57 +0000

    api changes

--- test.c
+++ test.c
@@ -34,25 +34,26 @@ int
 main (int argc, char ** argv)
 {
   avl_tree * tree;
+  unsigned int index;
 
   tree = avl_new_avl_tree (compare_longs, NULL);
 
-  avl_insert_by_key (tree, (void *) 50); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 45); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 15); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 10); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 75); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 55); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 70); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 80); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 60); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 32); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 20); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 40); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 25); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 22); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 31); avl_print_tree (tree, long_printer); avl_verify (tree);
-  avl_insert_by_key (tree, (void *) 30); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 50, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 45, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 15, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 10, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 75, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 55, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 70, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 80, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 60, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 32, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 20, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 40, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 25, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 22, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 31, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
+  avl_insert_by_key (tree, (void *) 30, &index); avl_print_tree (tree, long_printer); avl_verify (tree);
   while (tree->length) {
     int num = 0;
     int any = 0;
