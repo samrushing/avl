593161157b48d569996815ab269b61f4f1c10809
Sam Rushing <sam-github@rushing.nightmare.com>
2014-06-13 15:00:19 -0700

    cleaned up, prepared for move to github.

--- setup.py
+++ setup.py
@@ -1,33 +1,28 @@
 # -*- Mode: Python; coding: iso-8859-1 -*-
-"""
-distutil setup script
-"""
 
 from distutils.core import setup, Extension
 
-# set file generation umask, so that group gets write permissions on
-# generated files
-import os
-os.umask(2)
-
-src     = ['AVLmodule.c']
+src = ['AVLmodule.c']
 
 ext_macros = []
 #ext_macros = ['DEBUG_AVL']
 
-setup (name         = "avl",
-       version      = "2.1.3",
-       description  = "AVL Tree Objects for Python",
-       author       = 'Samual M Rushing',
-       author_email = "[hidden]",
-       maintainer   = "Berthold Höllmann",
-       maintainer_email = "hoel@gl-group.com",
-       license      = "BSD",
-       url          = 'http://www.nightmare.com/squirl/python-ext/avl/',
-       libraries    = [("avl", {"sources": ["avl.c"]})],
-       ext_modules  = [Extension('avl', src,
-                                 define_macros=ext_macros)])
-
-# Local Variables:;
-# compile-command:"python setup.py build";
-# End:;
+setup (
+    name         = "avl",
+    version      = "2.1.3",
+    description  = "AVL Tree Objects for Python",
+    author       = 'Samual M Rushing',
+    author_email = "[hidden]",
+    license      = "BSD",
+    url          = 'https://github.com/samrushing/avl',
+    libraries    = [("avl", {"sources": ["avl.c"]})],
+    ext_modules  = [
+        Extension (
+            'avl',
+            src,
+            define_macros=ext_macros,
+            library_dirs=['.'],
+            libraries=['avl']
+        )
+    ]
+)
